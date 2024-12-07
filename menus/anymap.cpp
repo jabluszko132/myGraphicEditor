e59e6ad19f690473ddccc7d9fb5fde671dbf1d95
Jan Nowacki <j.nowacki53@gmail.com>
2024-12-07 19:53:02 +0100

    fix memory leak

--- menus/anymap.cpp
+++ menus/anymap.cpp
@@ -18,23 +18,14 @@ template<typename T>
 AnyMap<T>::AnyMap(int width, int height, T** map){
     this->width = width;
     this->height = height;
-    this->map = new T*[height];
-    for(int i = 0; i < height; i++){
-        this->map[i] = new T[width];
-        for(int j = 0; j < width; j++){
-            this->map[i][j] = map[i][j];
-        }
-    }
+    this->map = map;
 }
 
 
 template<typename T>
 AnyMap<T>::~AnyMap(){
     for(int i = 0;i < this->height; i++){
-        for(int j = 0;j < this->width; j++){
-            delete this->map[i][j];
-        }
-        delete this->map[i];
+        delete [] this->map[i];
     }
-    delete this->map;
+    delete [] this->map;
 }
