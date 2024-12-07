dae97cd60f9cf55e7530c214330783c0c395de1d
Jan Nowacki <j.nowacki53@gmail.com>
2024-12-07 19:27:43 +0100

    finish pgm

--- menus/pgm.cpp
+++ menus/pgm.cpp
@@ -1,3 +1,50 @@
 #include "pgm.h"
 
-PGM::PGM() {}
+PGM::PGM(int width, int height): AnyMap<std::byte>(width, height){}
+PGM::PGM(int width, int height, std::byte** map): AnyMap<std::byte>(width, height, map) {}
+
+void PGM::save(std::ofstream s){
+    s<<this->header<<'\n';
+    s<<this->width<<' '<<this->height<<'\n';
+    s<<(int)this->colorMaxVal;
+    for(int i = 0;i < this->height; i++){
+        for(int j = 0;j < this->width; j++){
+            s<<(int)this->map[i][j]<<' ';
+        }
+        s<<'\n';
+    }
+}
+
+PGM PGM::fromFile(std::ifstream s){
+
+    std::string buffer;
+    int whitespacePos, prevWhitespacePos;
+
+    std::string header;
+    int width, height;
+    while(std::getline(s, buffer) && buffer[0] == '#'); //comment lines begin with #
+    std::getline(s,buffer); //skips the header
+    while(std::getline(s, buffer) && buffer[0] == '#'); //comment lines begin with #
+
+    whitespacePos = buffer.find(' ');
+    height = std::stoi(buffer.substr(0, whitespacePos - 1));
+    width = std::stoi(buffer.substr(buffer.find(whitespacePos + 1, buffer.length())));
+
+    std::byte** map = new std::byte*[height];
+    for(int i = 0; i < height; i++){
+        map[i] = new std::byte[width];
+        prevWhitespacePos = 0;
+        for(int j = 0; j < width; j++){ //because last position terminates in \n not space
+            for(int k = prevWhitespacePos + 1; k < buffer.length(); k++){
+                if(buffer[k] == ' ' || buffer[k] == '\t' || buffer[k] == '\n'){
+                    whitespacePos = k;
+                    break;
+                }
+            }
+            map[i][j] = (std::byte)(std::stoi(buffer.substr(prevWhitespacePos + 1, whitespacePos + 1)));
+            prevWhitespacePos = whitespacePos;
+        }
+    }
+
+    return PGM(width,height,map);
+}
--- menus/pgm.h
+++ menus/pgm.h
@@ -3,16 +3,19 @@
 
 #include "AnyMap.h"
 
-class PGM : public AnyMap
+class PGM : public AnyMap<std::byte>
 {
 public:
     int width;
     int height;
     const std::string header;
-    bool** map;
-    static PGM fromFile(std::ofstream s);
-
-    PGM();
+    const std::byte colorMaxVal{255};
+    std::byte** map;
+    static PGM fromFile(std::ifstream s);
+    void save(std::ofstream s);
+    PGM(int width, int height);
+    PGM(int width, int height, std::byte** map);
+    ~PGM();
 };
 
 #endif // PGM_H
